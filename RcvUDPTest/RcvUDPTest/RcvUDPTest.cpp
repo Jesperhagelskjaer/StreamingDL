6a56d4a773bcc765732e5dcde2f5fdece6ac9cd9
Jesperhagelskjaer <jhag@dandrite.au.dk>
2017-08-03 14:33:03 +0200

    Tested rcvUdpTest

--- RcvUDPTest/RcvUDPTest/RcvUDPTest.cpp
+++ RcvUDPTest/RcvUDPTest/RcvUDPTest.cpp
@@ -6,9 +6,19 @@
 #include <string.h>
 #pragma comment(lib, "Ws2_32.lib")
 
-#define UDP_PORT 27072
+//#define UDP_PORT 27072
+#define UDP_PORT 26090
 
-void error(const char *);
+
+void error(int sock, const char *msg)
+{
+	if (sock > 0) {
+		closesocket(sock);
+	}
+	WSACleanup();
+	perror(msg);
+	exit(0);
+}
 
 int main()
 {
@@ -22,32 +32,26 @@ int main()
 	char buf[1024];
 	unsigned short serverPort = UDP_PORT;
 	sock = socket(AF_INET, SOCK_DGRAM, 0);
-	if (sock < 0) error("Opening socket");
+	if (sock < 0) error(sock, "Opening socket");
 	length = sizeof(server);
 	memset(&server, 0, length);
 	server.sin_family = AF_INET;
 	server.sin_addr.s_addr = INADDR_ANY;
 	server.sin_port = htons(serverPort);
-	if (bind(sock, (struct sockaddr *)&server, length)<0) error("binding");
+	if (bind(sock, (struct sockaddr *)&server, length)<0) error(sock, "binding");
 	fromlen = sizeof(struct sockaddr_in);
 	while (1)
 	{
 		n = recvfrom(sock, buf, 1024, 0, (struct sockaddr *)&from, &fromlen);
-		if (n<0) error("recvfrom");
+		if (n<0) error(sock, "recvfrom");
 		//write(1,"Received a datagram: ", 21);
 		//write(1,buf,n);
 		printf("Received a datagram: %s", buf);
-		n = sendto(sock, "Got your message\n", 17, 0, (struct sockaddr *)&from, fromlen);
-		if (n<0)error("sendto");
+		//n = sendto(sock, "Got your message\n", 17, 0, (struct sockaddr *)&from, fromlen);
+		//if (n<0) error(sock, "sendto");
 	}
 	closesocket(sock);
 	WSACleanup();
 	return 0;
 }
 
-
-void error(const char *msg)
-{
-	perror(msg);
-	exit(0);
-}
